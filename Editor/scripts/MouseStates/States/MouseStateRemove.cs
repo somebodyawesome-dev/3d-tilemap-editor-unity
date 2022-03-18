032991edc8068fed4a213acbbc2f8e049449b007
somebodyawesome-dev <59267562+somebodyawesome-dev@users.noreply.github.com>
2022-03-19 00:04:49 +0100

    adding remove brush

---
+++ Editor/scripts/MouseStates/States/MouseStateRemove.cs
@@ -0,0 +1,29 @@
+﻿namespace Editor.scripts.MouseStates.States
+{
+    public class MouseStateRemove:MouseState
+    {
+        public MouseStateRemove(TileMap3d tile) : base(tile)
+        {
+        }
+
+        public override void onMouseClick()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnDrawGizmos()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void onFieldsUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void onDestroy()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
